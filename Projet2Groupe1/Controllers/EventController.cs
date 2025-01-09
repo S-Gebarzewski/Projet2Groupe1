d0439e7d51f28027de8e9fb2bfce17195aa89cea
Gebarzewski <solenegebarzewski@gmail.com>
2025-01-09 21:59:25 +0100

    add navigation structure

---
+++ Projet2Groupe1/Controllers/EventController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Projet2Groupe1.Controllers
+{
+    public class EventController : Controller
+    {
+        public IActionResult Catalog()
+        {
+            return View();
+        }
+
+        public IActionResult TicketDetail()
+        {
+            return View();
+        }
+
+        public IActionResult PurchaseTicket()
+        {
+            return View();
+        }
+    }
+}
