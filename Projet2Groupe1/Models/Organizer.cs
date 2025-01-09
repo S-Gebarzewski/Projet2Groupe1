b84090509598048f298b8080120816b696bd480c
Manel Merini <myloviche15@gmail.com>
2025-01-09 12:01:18 +0100

    Start_Css

--- Projet2Groupe1/Models/Organizer.cs
+++ Projet2Groupe1/Models/Organizer.cs
@@ -1,12 +1,21 @@
-﻿namespace Projet2Groupe1.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Projet2Groupe1.Models
 {
     public class Organizer
     {
         public int Id { get; set; }
-       public  string Function { get; set; }   
-       public  string Denomination { get; set; }
-        public string RIB { get; set; }   
-       public  Adress? Adress { get; set; }
+        [Required(ErrorMessage = "La fonction est obligatoire.")]
+        [StringLength(100, ErrorMessage = "La fonction ne peut pas dépasser 100 caractères.")]
+        public  string Function { get; set; }
+        [Required(ErrorMessage = "La dénomination sociale est obligatoire.")]
+        [StringLength(200, ErrorMessage = "La dénomination sociale ne peut pas dépasser 200 caractères.")]
+        public string Denomination { get; set; }
+        [Required(ErrorMessage = "Le RIB est obligatoire.")]
+        [RegularExpression(@"^[0-9A-Za-z]{27}$", ErrorMessage = "Le RIB doit comporter exactement 27 caractères alphanumériques.")]
+        public string RIB { get; set; }
+        
+        public  Adress? Adress { get; set; }
         public int UserId {  get; set; }    
         //String file = "C:\\Users\\Naciim\\Desktop\\Isika\\.NET\\Projet2Groupe1\\Projet2Groupe1\\wwwroot\\Documents\";
 
--- Projet2Groupe1/Models/User.cs
+++ Projet2Groupe1/Models/User.cs
@@ -1,13 +1,26 @@
-﻿namespace Projet2Groupe1.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Projet2Groupe1.Models
 {
     // cadre de mon user
     public class User
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "Le prenom est obligatoire.")]
+        [MinLength(2, ErrorMessage = "Le prenom doit contenir au moins 2 caracteres.")]
         public string FirstName { get; set; }
+        [Required(ErrorMessage = "Le nom de famille est obligatoire.")]
+        [MinLength(2, ErrorMessage = "Le nom de famille doit contenir au moins 2 caracteres.")]
         public string LastName { get; set; }
+        [Required(ErrorMessage = "Le numero de telephone est obligatoire.")]
+        [RegularExpression(@"^0[1-9](\d{2}){4}$", ErrorMessage = "Veuillez entrer un numéro de téléphone valide au format français (ex : 0612345678).")]
         public int Phone { get; set; }
+        [Required(ErrorMessage = "L'adresse mail est obligatoire.")]
+        [EmailAddress(ErrorMessage = "L'adresse mail doit avoir le format : exemple@exemple.com")]
         public string Mail { get; set; }
+        [RegularExpression("^[a-zA-Z!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~²³€£°§]{12,50}$", ErrorMessage = "Le mot de passe doit contenir au moins une majuscule, une minuscule et un caractere special.")]
+        [StringLength(50, MinimumLength = 12, ErrorMessage = "Le mot de passe doit comprendre entre 12 et 20 caracteres.")]
         public string Password { get; set; }
         public bool Newsletter { get; set; }
         public UserRole Role { get; set; } // appel a mon enum 
