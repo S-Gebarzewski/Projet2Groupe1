4f908c680d9512e69ef727f39288afb3a03f1c28
AnthonySk <anthony.statek@gmail.com>
2025-01-14 13:48:40 +0100

    add navigation dashboard to formService

---
+++ Projet2Groupe1/Controllers/ServiceController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Projet2Groupe1.Controllers
+{
+    public class ServiceController : Controller
+    {
+        public IActionResult CreateService()
+        {
+            return View();
+        }
+
+        public IActionResult MyServices()
+        {
+            return View();
+        }
+    }
+}
